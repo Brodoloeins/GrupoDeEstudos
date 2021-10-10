47d55bfbc44553db3444cf71eddb05d02c279d4c
Brodoloeins <joaofernandovascao@gmail.com>
2021-10-10 19:07:16 -0300

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
-// Imprimir 
+// Imprimir mensagem
+
+// console.log("Hello World")
+// alert("Hello World")
+
+// Variável
+
+// Variável não pode ter caracteres especiais a não ser "_", não pode começar com número, não pode ser uma palavra reservada e não pode ter espaço
+// let nomeDaVariavel = "valor da variável";
+
+// console.log(nomeDaVariavel)
+
+// nomeDaVariavel = "João"
+
+/*
+    int = número inteiro,
+    string = cadeia de caracteres,
+    bool = verdadeiro ou falso,
+    float = número com ponto flutuante
+*/
+
+// console.log(nomeDaVariavel)
+
+// Formas de declarar variável: const, let e var
+// var = variável para todo o código
+// let = variável para um escopo
+// const = variável para leitura
+
+// Concatenação
+
+var precoDoPao = 5.89
+
+// console.log("Preço do pão: R$" + precoDoPao.toFixed(2))
+
+// Condicionais
+
+// = recebe
+// == Com o mesmo valor
+// === Idêntico, precisa ser do mesmo tipo
+
+// console.log(typeof(precoDoPao))
+
+if(precoDoPao === "5.89"){
+
+    console.log("Isso é uma string")
+
+}else if(precoDoPao == "Olá"){
+
+    console.log(typeof(precoDoPao))
+
+}else{
+
+    console.log(typeof(precoDoPao)+", Isso")
+
+}
